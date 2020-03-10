c56f21942a8c26c94d3b18b185c7d88378d15869
root <reymer_n@outlook.com>
2020-03-10 10:26:39 -0400

    SK4

--- start.sh
+++ start.sh
@@ -4,21 +4,10 @@ set -e
 
 cd /data
 
-cp -rf /tmp/feed-the-beast/* .
 echo "eula=true" > eula.txt
 
-if [[ ! -e server.properties ]]; then
-    cp /tmp/server.properties .
-fi
-
-if [[ -n "$MOTD" ]]; then
-    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
-fi
-if [[ -n "$LEVEL" ]]; then
-    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
-fi
 if [[ -n "$OPS" ]]; then
     echo $OPS | awk -v RS=, '{print}' >> ops.txt
 fi
 
-java $JVM_OPTS -jar FTBserver-*.jar nogui
+java -server $JVM_OPTS -jar /data/forge-1.12.2-14.23.5.2838-universal.jar nogui
